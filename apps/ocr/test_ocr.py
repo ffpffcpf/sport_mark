3d90975195b498e373950fc1a3a6b2b624dfe4d8
Pengfei Chen <ffpffcpf@cmbchina.com>
2018-11-11 14:36:13 +0800

    test tesseract

---
+++ apps/ocr/test_ocr.py
@@ -0,0 +1,5 @@
+from PIL import Image
+import pytesseract
+from sport_mark.settings import BASE_DIR
+words = pytesseract.image_to_string(Image.open(BASE_DIR+'/media/test.jpg'), lang='chi_sim')
+print(words)
